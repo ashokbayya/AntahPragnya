5127fe4b9b84afa9b363e44f944e77c2e84619c9
Ashok Bayya <ashokbayya531@gmail.com>
2019-07-09 18:57:22 +0530

    Ashok
    first

---
+++ Home.php
@@ -0,0 +1,20 @@
+<html>
+	<head>
+		<title>AntahPragnya-2K19</title>
+		<link rel="stylesheet" type="text/css" href="css/Style_Home.css">
+	</head>
+	<body>
+		<div><h1 class="Title">AntahPragnya-2K19</h1></div>
+		<!--<div class="quote"><p>The clouds which give a charming rain are the production of burning hot sun. Innovation is only possible of the burnings hots deep inside</p></div>-->
+		<div id="foreground"></div>
+		<div class="Clouds">
+			<div class="cldbtn"><a href="http://antahpragnya.rgukt.ac.in/about/" id="about"><image src="css/images/about_cloud.png"></a></div>
+			<div class="cldbtn"><a href="http://antahpragnya.rgukt.ac.in/events/" id="events"><image src="css/images/events_cloud.png"></div>
+			<div class="cldbtn"><a href="" id="news"><image src="css/images/news_cloud.png"></a></div>
+			<div class="cldbtn"><a href="" id="highlights"><image width="200px" height="100px" src="css/images/highlights_cloud.png"></a></div>
+			<div class="cldbtn"><a href="" id="gallery"><image width="200px" height="100px" src="css/images/gallery_cloud.png"></a></div>
+
+		</div>
+		<div class="train"><image id="gogo" src="css/images/scheduletrain.png"></div>
+	</body>
+</html>
---
+++ LogIn.php
@@ -0,0 +1,27 @@
+<html>
+	<head>
+		<title>LogIn</title>
+		<link rel="stylesheet" type="text/css" href="css/Style_LogIn.css">
+	</head>
+	<body>
+		<div class="login_icon"><image src="css/images/login_icon.png"></div>
+		<div class="pointing_icon"></div>
+		<div class="box">
+			<form class="form" method="POST" action="" name="LogIn">
+				<br>
+				<input class="bar" type="text" name="UserName" placeholder="UserName">
+				<input class="bar"  type="password" name="Password" placeholder="Password"><br>
+				<input id="submit" type="submit" value="LogIn"><br>
+				<input type="checkbox" placeholder="Password" name="Remember_me" value="1">
+				Remember me.<br>
+			</form>
+			<div class="user_icon">
+				<image id="user_icon" src="css/images/username_icon.jpg">
+			</div>
+			<div class="paasword_icon">
+				<image id="password_icon" src="css/images/password_icon.jpg">
+			</div>
+			<div class="forget"><a href="">Forgot Password?</a></div>
+		</div>
+	</body>
+</html>
